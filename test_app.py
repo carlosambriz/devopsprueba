272f152e1208d231aaf5be728e091ad7d90a547d
carlosambriz <32373913+carlosambriz@users.noreply.github.com>
2025-02-27 15:32:29 -0600

    Create test_app.py
    archivo para  invocar   el app de  prueba en python y  ingresar  daros personalizados  se trata de una suma basica  y este seria como el main para invocar la funcion y usarla

---
+++ test_app.py
@@ -0,0 +1,9 @@
+import unittest
+from app import suma
+
+class TestApp(unittest.TestCase):
+    def test_suma(self):
+        self.assertEqual(suma(2, 3), 5)
+
+if __name__ == "__main__":
+    unittest.main()
